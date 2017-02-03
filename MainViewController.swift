bf60a5ef4e7920206b0502b9e1ac582ffa6343bd
Ilias Basha <iliasbasha@Iliass-MacBook-Pro.local>
2017-02-02 18:16:32 -0700

    Main View, buttons _ labels added. Background 50% done.

---
+++ MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  Maralog
+//
+//  Created by Ilias Basha on 2/2/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
