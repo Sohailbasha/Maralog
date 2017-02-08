3ae498d06182e4e12c5c2495176f28bac4b19885
Ilias Basha <iliasbasha@Iliass-MacBook-Pro.local>
2017-02-07 17:48:46 -0700

    added Core Location and about to start geocoder

---
+++ Maralog/Location + Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Location + Convenience.swift
+//  Maralog
+//
+//  Created by Ilias Basha on 2/7/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
---
+++ Maralog/LocationController.swift
@@ -0,0 +1,9 @@
+//
+//  LocationController.swift
+//  Maralog
+//
+//  Created by Ilias Basha on 2/7/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
