1906aa47f4ec1c98148a92d08fcd06c72b505b4c
Sohailbasha <shlbsh94@gmail.com>
2017-03-15 18:18:23 -0600

    continuing with tutorial

--- Maralog/OnboardingViewController.swift
+++ Maralog/OnboardingViewController.swift
@@ -29,7 +29,7 @@ class OnboardingViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func continueTouched(_ sender: Any) {
             UserController.sharedInstance.saveUserName(name: nameTextField.text)
-            performSegue(withIdentifier: "toTutorialSegue", sender: self)
+            performSegue(withIdentifier: "toMainSegue", sender: self)
     }
 
     
