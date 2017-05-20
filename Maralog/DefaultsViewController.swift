a9fc78e6c4b22b303cde07201584bc770947c570
Sohailbasha <shlbsh94@gmail.com>
2017-05-19 21:33:01 -0400

    feature complete

--- Maralog/DefaultsViewController.swift
+++ Maralog/DefaultsViewController.swift
@@ -27,8 +27,6 @@ class DefaultsViewController: UIViewController {
     // MARK: - Actions
     
     
-    
-
 }
 
 
--- Maralog/MoreOptionsViewController.swift
+++ Maralog/MoreOptionsViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class MoreOptionsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.delegate = self
         tableView.dataSource = self
         
         let name = UserController.sharedInstance.getName()
         self.yourNameLabel.text = "Your Name: \(name)"
+        
+        self.changeDesignsFor(buttons: [saveNameButton, watchWalkthroughButton])
     }
-
+    
     
     // MARK: - Outlets
     
@@ -29,17 +31,28 @@ class MoreOptionsViewController: UIViewController {
     
     @IBOutlet var nameChangeTextField: UITextField!
     
+    @IBOutlet var saveNameButton: UIButton!
+    
+    @IBOutlet var watchWalkthroughButton: UIButton!
+    
     
     // MARK: - Actions
-    
     @IBAction func saveNameButtonTapped(_ sender: Any) {
+        let color = UIColor(red: 137/255, green: 216/255, blue: 144/255, alpha: 1)
         UserController.sharedInstance.saveUserName(name: nameChangeTextField.text)
-        
+        UIView.animate(withDuration: 1, animations: {
+            self.saveNameButton.backgroundColor = color
+            self.saveNameButton.setTitleColor(.white, for: .normal)
+            self.saveNameButton.setTitle("Saved!", for: .normal)
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, animations: {
+                self.saveNameButton.backgroundColor = .clear
+                self.saveNameButton.setTitleColor(.black, for: .normal)
+                self.saveNameButton.setTitle("Save", for: .normal)
+            })
+        }
     }
-    
-   
 }
-
 
 extension MoreOptionsViewController: UITableViewDataSource, UITableViewDelegate {
     
@@ -55,6 +68,22 @@ extension MoreOptionsViewController: UITableViewDataSource, UITableViewDelegate 
     }
 }
 
+
+extension MoreOptionsViewController {
+    
+    
+    func changeDesignsFor(buttons: [UIButton]) {
+        for button in buttons {
+            button.backgroundColor = .clear
+            button.layer.cornerRadius = 5
+            button.layer.borderWidth = 0.5
+            button.layer.borderColor = UIColor.black.cgColor
+            
+            button.setTitleColor(.black, for: .normal)
+        }
+    }
+}
+
 extension MoreOptionsViewController: UITextFieldDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -66,3 +95,6 @@ extension MoreOptionsViewController: UITextFieldDelegate {
         return true
     }
 }
+
+
+
