7b292ab43bd854a01635fd84ec7cc88efa8820c6
Sohailbasha <shlbsh94@gmail.com>
2017-05-20 16:14:29 -0400

    added new custom class
    Added a custom class for UICollectionViewCell for the initial sign in page. Every different page will have to be it’s own custom collectionview cell.

---
+++ Maralog/LoginCell.swift
@@ -0,0 +1,70 @@
+//
+//  LoginCell.swift
+//  Maralog
+//
+//  Created by Ilias Basha on 5/20/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class LoginCell: UICollectionViewCell {
+    
+    let logoImageView: UIImageView = {
+        let image = UIImage(named: "logo")
+        let imageView = UIImageView(image: image)
+        return imageView
+    }()
+    
+    let enterUsernameTextField: leftPaddedTextField = {
+        let textfield = leftPaddedTextField()
+        textfield.layer.borderColor = UIColor.lightGray.cgColor
+        textfield.layer.borderWidth = 0.5
+        textfield.placeholder = " enter your name to start"
+        textfield.keyboardType = .alphabet
+        textfield.keyboardAppearance = .dark
+        return textfield
+    }()
+    
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .red
+        button.setTitle("start", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(logoImageView)
+        addSubview(enterUsernameTextField)
+        addSubview(loginButton)
+        
+        _ = logoImageView.anchor(centerYAnchor, left: nil, bottom: nil, right: nil, topConstant: -230, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 100, heightConstant: 100)
+        logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        
+        _ = enterUsernameTextField.anchor(logoImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 20, leftConstant: 32, bottomConstant: 0, rightConstant: 32, widthConstant: 0, heightConstant: 50)
+        
+        _ = loginButton.anchor(enterUsernameTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 8, leftConstant: 32, bottomConstant: 0, rightConstant: 32, widthConstant: 0, heightConstant: 50)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+class leftPaddedTextField: UITextField {
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 5, y: bounds.origin.y, width: bounds.width + 10, height: bounds.height)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 5, y: bounds.origin.y, width: bounds.width + 10, height: bounds.height)
+    }
+}
