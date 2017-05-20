c4b2ee86e04811f8b40daf6f0c6a2163f20a9d54
Sohailbasha <shlbsh94@gmail.com>
2017-05-20 16:02:03 -0400

    added classes
    New class - Page, to help create each page that will be reused in tutorial VC.

---
+++ Maralog/Page.swift
@@ -0,0 +1,9 @@
+//
+//  Page.swift
+//  Maralog
+//
+//  Created by Ilias Basha on 5/20/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
--- Maralog/SignInViewController.swift
+++ Maralog/SignInViewController.swift
@@ -8,28 +8,33 @@
 
 import UIKit
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        cv.dataSource = self
+        cv.delegate = self
+        cv.isPagingEnabled = true
+        return cv
+    }()
+    
+    let cellId = "cellId"
+    let loginCellId = "loginCellId"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
+
+  
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
