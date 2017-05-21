92638a83f661d48eeaeb107424aafa5b57b5550c
Sohailbasha <shlbsh94@gmail.com>
2017-05-21 13:22:42 -0400

    removed gradientview
    doesn’t fit the project well. maybe another one.

--- Maralog/LoginCell.swift
+++ Maralog/LoginCell.swift
@@ -36,19 +36,9 @@ class LoginCell: UICollectionViewCell {
     }()
     
     
-    let animationView: UIViewX = {
-        let view = UIViewX()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    var currentColorArrayIndex = -1
-    var colorArray: [(color1: UIColor, color2: UIColor)] = []
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-//        addSubview(animationView)
+
         addSubview(logoImageView)
         addSubview(enterUsernameTextField)
         addSubview(loginButton)
@@ -60,36 +50,9 @@ class LoginCell: UICollectionViewCell {
         _ = enterUsernameTextField.anchor(logoImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 20, leftConstant: 32, bottomConstant: 0, rightConstant: 32, widthConstant: 0, heightConstant: 50)
         
         _ = loginButton.anchor(enterUsernameTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 8, leftConstant: 32, bottomConstant: 0, rightConstant: 32, widthConstant: 0, heightConstant: 50)
-        
-        _ = animationView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: frame.width, heightConstant: frame.height)
-        
-        
-        
-        colorArray.append((color1: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), color2: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1), color2: #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), color2: #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), color2: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1), color2: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), color2: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
-        
-//        animateBackgroundColor()
-
 
     }
     
-    func animateBackgroundColor() {
-        currentColorArrayIndex = currentColorArrayIndex == (colorArray.count - 1) ? 0 : currentColorArrayIndex + 1
-        
-        UIView.transition(with: animationView, duration: 2, options: .transitionCrossDissolve, animations: { 
-            self.animationView.firstColor = self.colorArray[self.currentColorArrayIndex].color1
-            self.animationView.secondColor = self.colorArray[self.currentColorArrayIndex].color2
-        }) { (success) in
-            self.animateBackgroundColor()
-        }
-        
-    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
