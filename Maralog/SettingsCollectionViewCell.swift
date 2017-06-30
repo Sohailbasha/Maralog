f3bf6ddebb05c4c4307c21674426fc01f2c2b734
Sohailbasha <shlbsh94@gmail.com>
2017-06-30 10:49:48 -0400

    Added Collection View
    added uicolelctionview to Options VIew Controller.
    Deleted files associated with Symbols.
    About to create UICollectionViewCell.

---
+++ Maralog/SettingsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsCollectionViewCell.swift
+//  Maralog
+//
+//  Created by Ilias Basha on 6/30/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCollectionViewCell: UICollectionViewCell {
+    
+}
