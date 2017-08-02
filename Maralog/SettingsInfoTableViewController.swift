0dbafb5cce60a4b833c04d1128ff99ec7632ae22
Sohailbasha <shlbsh94@gmail.com>
2017-08-02 13:13:20 -0400

    got rid of redundant code in SettingsVC

--- Maralog/SettingsInfoTableViewController.swift
+++ Maralog/SettingsInfoTableViewController.swift
@@ -10,49 +10,30 @@ import UIKit
 import Foundation
 
 class SettingsInfoTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let setting = self.setting else {
-            return }
-        
-        self.updateDetailWith(setting)
-        self.title = setting.name
+        if let setting = self.setting {
+            self.updateDetailWith(setting)
+            self.title = setting.name
+        }
     }
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        
-        guard let setting = self.setting else {
-            return }
-        self.updateDetailWith(setting)
-        settingSwitch.isOn = setting.isOn
-        self.tableView.reloadData()
-    
-    }
-    
-    
-    
-    
+    // MARK: - Properties
     
     var setting: Settings?
     
-    var delegate: SwitchSettingsDelegate?
-    
     var switchStatus = Bool()
     
-
+    
     func updateDetailWith(_ setting: Settings) {
         imageView.image = setting.icon
         textView.text = setting.description
         settingSwitch.isOn = setting.isOn
     }
     
-    func checkSettings() {
-        
-    }
     
     // MARK: - Outlets
     
@@ -68,15 +49,8 @@ class SettingsInfoTableViewController: UITableViewController {
         if let setting = self.setting {
             setting.isOn = sender.isOn
             SettingsController.sharedInstance.saveAsDefault(setting: setting, value: sender.isOn)
-            
-            self.tableView.reloadData()
         }
     }
 }
 
 
-
-protocol SwitchSettingsDelegate: class {
-    
-    func captureDefaultSettingFor(setting: Settings, selected: Bool)
-}
