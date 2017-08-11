661e63b69a46ccf1570844723fa9736ddcaae067
Sohailbasha <shlbsh94@gmail.com>
2017-08-10 20:49:37 -0400

    added new files for subclassing.

---
+++ Maralog/Experimental/NewContactViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewContactViewController.swift
+//  Maralog
+//
+//  Created by Ilias Basha on 8/10/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class NewContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
